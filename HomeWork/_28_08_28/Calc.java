4c387eba0d4c0418f5a1724612020483559e3b9f
Ivan.Burniashev <kbk@ukr.net>
2023-09-09 20:30:28 +0200

    add hw 28.08.23 and field ClassWork

--- HomeWork/_28_08_28/Calc.java
+++ HomeWork/_28_08_28/Calc.java
@@ -8,25 +8,37 @@ public class Calc {
     }
 
     public double average() {
-        int average = 0;
-        int index = 0;
+        double average = 0;
+        int count = 0;
         for (int element : Array) {
             if (element > 0) {
                 average += element;
-                index += 1;
+                count += 1;
             }
         }
-        return average / index;
+        return average / count;
     }
 
-//    public int summ() {
-//        int summ = 0;
-//        for (int element : Array
-//        ) {
-//            if (element == null) {
-//
-//            }
-//
-//        }
-//    }
+
+    public int summ() {
+        int summ = 0;
+        for (int element : Array) {
+            if (element % 3 == 0 && element % 5 != 0) {
+                summ += element;
+            }
+
+        }
+        return summ;
+    }
+
+    public int maxSumm() {
+        int maxSumm = 0;
+        for (int i = 0; i < Array.length - 1; i++) {
+            if (Array[i] + Array[i + 1] > maxSumm) {
+                maxSumm = Array[i] + Array[i + 1];
+            }
+        }
+        return maxSumm;
+    }
+
 }
--- HomeWork/_28_08_28/Main.java
+++ HomeWork/_28_08_28/Main.java
@@ -3,12 +3,16 @@ package JavaProLectures.HomeWork._28_08_28;
 public class Main {
     public static void main(String[] args) {
         /*
-         1 уровень сложности: Найти среднее арифметическое положительных элементов массива.
-Найти сумму всех элементов в массиве, которые делятся на 3 и одновременно не делятся на 5.
-Найти в массиве наибольшую сумму подряд идущих элементов.
+         1 уровень сложности:
+            - Найти среднее арифметическое положительных элементов массива.
+            - Найти сумму всех элементов в массиве, которые делятся на 3 и одновременно не делятся на 5.
+            - Найти в массиве наибольшую сумму двух подряд идущих элементов.
          */
-        int[] Array = {-3, 5, 10, 1, -4, 3, 9, 6, 12, -15, 30};
+        int[] Array = {-3, 5, 10, 1, -4, 3, 9, 6, 12, -15, 30, 100};
         Calc calc = new Calc(Array);
-        System.out.printf(""calc.average());
+//        System.out.printf(""calc.average());
+        System.out.println(Math.floor(calc.average()));
+        System.out.println(calc.summ());
+        System.out.println(calc.maxSumm());
     }
 }
