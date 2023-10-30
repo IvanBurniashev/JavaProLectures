898aaee7c4db978b53f7155dd925a41562ff998d
IvanBurniashev <kbk@ukr.net>
2023-10-30 22:52:22 +0100

    add 30.10.23

---
+++ src/main/java/org/example/HomeWork/_2023_10_30/Main.java
@@ -0,0 +1,19 @@
+package org.example.HomeWork._2023_10_30;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(10,20));
+        System.out.println(calculator.subtract(10,20));
+        System.out.println(calculator.multiply(10,20));
+        System.out.println(calculator.divide(200,20));
+        System.out.println(calculator.power(1,0));
+        System.out.println(calculator.factorial(5));
+        System.out.println(calculator.findMax(new int[] {1,0,10,5,11}));
+        System.out.println(calculator.isPrime(53));
+        System.out.println("==========================");
+        System.out.println(calculator.reverseString("abcd"));
+        System.out.println(calculator.productOfArray(new int[]{1, 2, 3, 4}));
+    }
+}
