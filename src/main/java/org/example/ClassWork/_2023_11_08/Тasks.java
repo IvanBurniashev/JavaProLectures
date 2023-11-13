6d5565b24ea506e21c6bf094e3312c0e22308582
IvanBurniashev <kbk@ukr.net>
2023-11-13 14:18:48 +0100

    add cw _2023_11_08

---
+++ src/main/java/org/example/ClassWork/_2023_11_08/Тasks.java
@@ -0,0 +1,27 @@
+package org.example.ClassWork._2023_11_08;
+
+public class Тasks {
+//две строки сравнение, вторая с решетками
+    //1. Найти решетки до букв и удалить их
+    //2. Если перед р. есть буква, тогда нужно подсчитать кол-во р. и удалить то же кол-во букв
+    //стоящих перед решеткой.
+    //3. Итоговые строки сравнить.
+    public static void twoStringComparison(String a, String b){
+
+        System.out.println(b.indexOf("#"));
+    }
+
+    /**
+     * s= "LDLDRURU"
+     * Вернётся ли в исходное положение
+     * param args
+     */
+    public boolean cursor(String a){
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        twoStringComparison("","fjk#jkl");
+    }
+}
